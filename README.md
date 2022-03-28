4bbb26076ff7881ecfb1fbb53c974bc573299ec2
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-03-28 20:39:53 +0200

    prisoners

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Combinatories
  Working on some math problems related to combinatories and probabilities.
+
+The prisoners problem: 
+A bunch of numbered prisoners is asked to individually look for its number inside several boxes, as well numbered, which contain a card from a deck.
+They can search in up to half of the number of prisoners boxes.
+The most valuable strategy is that they first search in its own number box.
+In case it does not match its number they should go next to the box with the number of the card they just look at.
+Doing this increases the probabilities up to roughly 35% with 10 prisoners.
---
+++ prisoners.py
@@ -0,0 +1,45 @@
+import math
+import itertools
+
+def loop_idx(arr, idx):
+	# Given a value of the array it searchs for the size of the loop
+	# The loop ends if the value is equal to the index
+	# Or if we loop through a repeated index
+    steps = 0
+    memo_idxs = []
+    # Main loop
+    while True:
+        val = arr[idx]
+        steps += 1
+        if idx == val:
+            return steps
+        if idx in memo_idxs:
+            return steps - 1
+        memo_idxs.append(idx)
+        idx = val
+
+
+def fun(arr):
+	# Loop for the elements of each posibilitiy
+	# Check each index for the greatest loop
+	# If it's bigger than half the length of the array, it is a bad scenario
+    for idx,val in enumerate(arr):
+        steps = loop_idx(arr, idx)
+        if steps > len(arr)/2:
+            return 1
+    return 0
+
+
+def main(n):
+	# Calculate all total cases: both number and permutations physically
+	result = 0
+	posibilities = math.factorial(n)
+	comb = itertools.permutations(range(n),n)
+	for x in comb:
+		result += fun(x)
+	# Computate percentage of failure cases
+	percentage = (1-(result/posibilities))*100
+	total = format(percentage, '.2f')
+	print(f'The percentage of surviving is: {total} % \n{posibilities-result} favorable cases out of {posibilities} total cases.')
+
+main(8)
